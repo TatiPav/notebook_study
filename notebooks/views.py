2e4355a96b3a95497e210a712a70733aaa5038c3
TatiPav <64731979+tatipav@users.noreply.github.com>
2021-06-09 22:35:26 +0300

    work

---
+++ notebooks/views.py
@@ -0,0 +1,12 @@
+# from django.shortcuts import render
+# from .models import Topic
+#
+# # Create your views here.
+# def index(request):
+#     return render(request, 'notebooks/index.html')
+#
+# def topics(request):
+#     topics = Topic.objects.order_by('date_added')
+#     context = {'topics': topics}
+#     return render(request, 'notebooks/topics.html', context)
+
